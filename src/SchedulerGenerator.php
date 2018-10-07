68cdf481f87e86e516c4e0f785ef65b4ba21ac56
Andrey Selikov <andreyselikov@MacBook-Pro-Andrey.local>
2018-10-07 16:55:31 +0300

    use priority for create schedule

--- src/SchedulerGenerator.php
+++ src/SchedulerGenerator.php
@@ -5,7 +5,7 @@ namespace Schedule;
 use \DateTime;
 use \DateInterval;
 use \DatePeriod;
-use \Exception;
+use Exception\SchedulerException;
 use Schedule\ISchedulerGenerator;
 
 /**
@@ -15,224 +15,250 @@ use Schedule\ISchedulerGenerator;
  * @author  Andrey Selikov <selffmail@gmail.com>
  * @since   November 10, 2016
  * @link    https://github.com/selff/Scheduler
- * @version 1.0.1
+ * @version 1.0.2
  */
-
 class SchedulerGenerator implements ISchedulerGenerator
 {
 
     /**
      * DateTime periods of breaks, with coma separate
      * @var array(DataTime $start,DataTime $end)
-     */	
-	public $breaks;
-		
-	/**
+     */
+    public $breaks;
+
+    /**
      * DatePeriod (begin event,slot interval,end event)
      * @var DatePeriod
      */
-	public $daterange;
+    public $daterange;
 
-	/**
-     * Some options 
+    /**
+     * Some options
      * @var array
      */
-	public $settings = array(
-		'initial_preset' => true,
-		'find_initial_time' => false,
-		'initial_format' => 'H:i:s',
-		);
+    public $settings = array(
+        'initial_preset' => true,
+        'find_initial_time' => false,
+        'initial_format' => 'H:i:s',
+    );
 
-	/**
-     * Result columns of output table 
+    /**
+     * Result columns of output table
      * @var array
      */
-	private $columns = array();
+    protected $columns = array();
 
-	/**
-     * Result rows of output table 
+    /**
+     * Result rows of output table
      * @var array
      */
-	private $rows = array();
+    protected $rows = array();
 
-	/**
-     * Prepare output table 
+    /**
+     * Prepare output table
      * @var array
      */
-	private $schedule = array();
+    protected $schedule = array();
 
 
     /**
-     * Generate shedule data
+     * Generate schedule data
      *
-     * @param void
-	 */	
-	public function generateSchedule($data){
+     * @param array with input data
+     * @return array with schedule result
+     */
+    public function generateSchedule($data)
+    {
 
-		$this->loadData($data);
-		$this->initTime();
-		$this->fillSlots();
-		return $this->generateResultGrid();
-	}
+        $this->loadData($data);
+
+        if (empty($this->rows) || empty($this->columns)) {
+            throw new SchedulerException('Input data does not contain the required data');
+        }
+
+        $this->initTime();
+
+        $this->createSchedule();
+
+        if (empty($this->schedule)) {
+            throw new SchedulerException('Error create schedule for data you entered');
+        }
+
+        return $this->generateResultGrid();
+    }
 
     /**
      * Load input data as dummy table and initialize $columns and $rows for output
      *
-     * @param array  $rows
-	 */
-	public function loadData($rows){
+     * @param array $rows
+     * @return void
+     */
+    public function loadData($rows)
+    {
 
-		foreach ($rows as $i=>$row) {
-			// по столбцам
-			foreach($row as $k=>$v) {
-				// первая строка содержит названия компаний
-				if (0 == $i) {
-					if ($k>2) $this->columns[($k-3)]['name'] = $v;
-				// вторая строка содержит кол-во персонала компаний
-				} elseif (1 == $i) {
-					if ($k>2) $this->columns[($k-3)]['pers'] = $v ? (int)$v : 1;
-				// начиная с третьей строки 
-				} else {
-					switch ($k) {
-						case 0:
-							$this->rows[($i-1)]['name'] = $v; 
-							break;
-						case 1: 
-							$this->rows[($i-1)]['type'] = $v; 
-							break;
-						case 2:
-							$this->rows[($i-1)]['face'] = $v; 
-							break;
-						default:
-							$this->rows[($i-1)]['intersection'][($k-3)] = $v;	
-							break;
-					}
-				}
-			}
-		}
-		if (empty($this->rows) || empty($this->columns)) {
-			throw new SchedulerException('Input data does not contain the required data');
-		}
-	}
+        // по строкам
+        foreach ($rows as $i => $row) {
+            // по столбцам
+            foreach ($row as $k => $v) {
+                // первая строка содержит названия компаний
+                if (0 == $i) {
+                    if ($k > 3) $this->columns[($k - 4)]['name'] = $v;
+                    // вторая строка содержит кол-во персонала компаний
+                } elseif (1 == $i) {
+                    if ($k > 3) $this->columns[($k - 4)]['persons'] = $v ? (int)$v : 1;
+                    // начиная с третьей строки
+                } else {
+                    switch ($k) {
+                        case 0:
+                            $this->rows[($i - 1)]['name'] = $v;
+                            break;
+                        case 1:
+                            $this->rows[($i - 1)]['type'] = $v;
+                            break;
+                        case 2:
+                            $this->rows[($i - 1)]['priority'] = (int)$v;
+                            break;
+                        case 3:
+                            $this->rows[($i - 1)]['face'] = $v;
+                            break;
+                        default:
+                            $this->rows[($i - 1)]['intersection'][($k - 4)] = $v;
+                            break;
+                    }
+                }
+            }
+        }
+    }
 
     /**
      * Init time slots in output shedule table
      *
      * @param void
-	 */
-	private function initTime(){
-    	$j=0;
-    	foreach($this->daterange as $i=>$date){
-    		
-    		// find break periods
-    		$break_times = array();
-	    	foreach ($this->breaks as $break) {
-	    		if ($date >= $break['start'] && $date < $break['end']) {
-	    			$break_times[] = $date->format($this->settings['initial_format']);
-	    		}
-			}
+     * @return void
+     */
+    protected function initTime()
+    {
+        $j = 0;
+        foreach ($this->daterange as $i => $date) {
 
-			// if time not in a break_times - put to schedule
-			if (!in_array($date->format($this->settings['initial_format']),$break_times)) {
-				
-				$this->schedule[$j]['time'] = $date->format($this->settings['initial_format']);
-		    	$this->schedule[$j]['periods'] = array();	
-		    	$j++;
-		    }
-				
-		}
+            // find break periods
+            $break_times = array();
+            foreach ($this->breaks as $break) {
+                if ($date >= $break['start'] && $date < $break['end']) {
+                    $break_times[] = $date->format($this->settings['initial_format']);
+                }
+            }
 
-		if ($this->settings['initial_preset']) {
-	    	foreach($this->daterange as $i=>$date){
-			    foreach($this->rows as $j=>$intersection) {
-			    	foreach($intersection['intersection'] as $k=>$value) {
-			    		if ($value == $date->format($this->settings['initial_format'])) {
-			    			$this->schedule[$i]['periods'][] = array('columnValue' => $k,'rowValue' => $j);
-			    			$this->settings['find_initial_time'] = true;
+            // if time not in a break_times - put to schedule
+            if (!in_array($date->format($this->settings['initial_format']), $break_times)) {
 
-						}
-			    	}
-			    }
-			}
-		}
-	}
-    
+                $this->schedule[$j]['time'] = $date->format($this->settings['initial_format']);
+                $this->schedule[$j]['periods'] = array();
+                $j++;
+            }
+
+        }
+
+        if ($this->settings['initial_preset']) {
+            foreach ($this->daterange as $i => $date) {
+                foreach ($this->rows as $j => $intersection) {
+                    foreach ($intersection['intersection'] as $k => $value) {
+                        if ($value == $date->format($this->settings['initial_format'])) {
+                            $this->schedule[$i]['periods'][] = array('columnValue' => $k, 'rowValue' => $j);
+                            $this->settings['find_initial_time'] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     /**
-     * Fill shedule table
+     * Create shedule table
      *
      * @param void
-	 */	
-	private function fillSlots(){
+     * @return void
+     */
+    protected function createSchedule()
+    {
+        // for priority=1
+        $this->fillSlots(1);
+        // for priority=0
+        $this->fillSlots(0);
+    }
 
-    	foreach($this->rows as $user=>$intersection) {
-		   	// найдем период в котором указан маркер
-		    foreach ($intersection['intersection'] as $column=>$marker) { 
-		    	$marker = trim($marker);
+    /**
+     * Fill shedule table for priority
+     *
+     * @param void
+     * @return void
+     */
+    protected function fillSlots($priority)
+    {
+        foreach ($this->rows as $user => $row) {
+            // найдем период в котором указан маркер
+            foreach ($row['intersection'] as $column => $marker) {
+                $marker = trim($marker);
+                // начнем заполнять все ячейки которые помечены маркером
+                if (strlen($marker) < 3 && ($marker) && $row['priority']==$priority) {
+                    $this->fillSlot($user, $column);
+                }
+            }
+        }
+    }
 
-		    	// начнем заполнять все ячейки которые помечены маркером
-		    	if (strlen($marker)<3 && ($marker)) {
+    protected function fillSlot($user, $column)
+    {
+        $notSaved = true;
+        $currentPeriod = $persons = 0;
+        $iteration = 0;
+        $ratio = $this->columns[$column]['persons'];
+        $i = 0;
+        do {
 
-			    	$notsaved = true; 
-			    	$startPeriod = $currentPeriod = $pers = 0;
-			    	$prohod = 0;
-			    	$koef = $this->columns[$column]['pers'];
+            $interupt = false;
+            $periodBusyComp = false;
+            $periodBusyUser = false;
+            // проверим может этот интервал уже использован
+            if (isset($this->schedule[$currentPeriod])) {
+                if (count($this->schedule[$currentPeriod]['periods'])) {
+                    foreach ($this->schedule[$currentPeriod]['periods'] as $k => $period) {
 
-		    		for($z=0;$z<$koef;$z++){
+                        // в данный период пользователь уже использован
+                        if ($period['rowValue'] == $user) {
+                            $periodBusyUser = true;
+                        }
 
-		    		}
+                        // в данный период слот компании использован
+                        if ($period['columnValue'] == $column
+                            && ($iteration < $this->countPersBusyForPeriod($currentPeriod, $column))) {
+                            $periodBusyComp = true;
+                        }
+                    }
+                }
+            }
+            if (!$periodBusyUser && !$periodBusyComp) {
+                $this->schedule[$currentPeriod]['periods'][] = array(
+                    'columnValue' => $column, 'rowValue' => $user
+                );
+                $notSaved = false;
+            }
 
-			    	$i=0;
+            if (($i == (count($this->schedule) * $ratio - 1)) && $notSaved) {
+                //Not found slot for user={$user}, column={$column}
+                $interupt = true;
+            }
 
-			    	do {
+            $i++;
+            $currentPeriod++;
 
-			    		$interupt = false;
-						
-			    		$periodBusyComp = false; $periodBusyUser = false; 
-			    		// проверим может этот интервал уже использован
-			    		if (isset($this->schedule[$currentPeriod])) {
-							if (count($this->schedule[$currentPeriod]['periods'])) {
-			   					foreach($this->schedule[$currentPeriod]['periods'] as $k=>$period) {
-			   						// в данный период пользователь уже использован
-			   						if ( $period['rowValue'] == $user ) { 
-			   							$periodBusyUser = true; 
-			   						}
-			   						// в данный период слот компании использован
-			   						if ( $period['columnValue'] == $column 
-			   							&& ($prohod < $this->countPersBusyForPeriod($currentPeriod,$column) ) )  { 
-			   								$periodBusyComp = true; 
-			   						}
-			   					}
-			   				}
-			    		}
-			    		if (!$periodBusyUser && !$periodBusyComp )  {
-			    			$this->schedule[$currentPeriod]['periods'][] = array(
-			   					'columnValue' => $column,'rowValue' => $user
-			   				);
-			   				$notsaved = false;
-			    		}
-			    		
-			    		if ( ( $i == (  count($this->schedule)*$koef - 1) ) && $notsaved ) {
-			    			//Not found slot for user={$user}, column={$column}
-			    			$interupt = true;
-			    		}
+            if ($currentPeriod == count($this->schedule)) {
+                $currentPeriod = 0;
+                $iteration++;
+                //if ($this->countPersBusyForPeriod($currentPeriod, $column) > $persons) $persons++;
+            }
 
-			    		$i++; $currentPeriod++;
-			    		
-			    		if ($currentPeriod == count($this->schedule)) {
-			    			$currentPeriod = 0;
-			    			$prohod++;
-			    			if ($this->countPersBusyForPeriod($currentPeriod,$column) > $pers) $pers++;
-			    		}
-
-			    	} while ($notsaved && !$interupt);
-
-			    }
-			}
-		}
-   		
-   		if (empty($this->schedule)) {
-   			throw new SchedulerException('Failed to schedule the data you entered');
-   		}
+        } while ($notSaved && !$interupt);
     }
 
     /**
@@ -240,61 +266,66 @@ class SchedulerGenerator implements ISchedulerGenerator
      *
      * @param int $currentPeriod
      * @param int $column
-	 */		
-	private function countPersBusyForPeriod($currentPeriod, $column) {
-    	$counter = 0;
-    	$periods = $this->schedule[$currentPeriod];
-    	foreach($periods['periods'] as $period) {
-	    	if ($period['columnValue'] == $column) {
-	    		$counter++;
-	    	}
-    	}
-    	return $counter;
+     * @return int
+     */
+    protected function countPersBusyForPeriod($currentPeriod, $column)
+    {
+        $counter = 0;
+        $periods = $this->schedule[$currentPeriod];
+        foreach ($periods['periods'] as $period) {
+            if ($period['columnValue'] == $column) {
+                $counter++;
+            }
+        }
+        return $counter;
     }
 
     /**
      * Fill shedule table
      *
-     * @param array $data
-	 */	
-    public function generateResultGrid(){
-    	$data = array();
-    	$row = array('Company','Type','User');
-    	foreach($this->columns as $c) {
-    		$row[] = $c['name'];
-    	}
-    	$data[] = $row;
-    	$row = array('persons:','','');
-    	foreach($this->columns as $c) {
-    		$row[] = $c['pers'];
-    	}
-    	$data[] = $row;
-    	foreach($this->rows as $compkey=>$company) {
-    		$row = array();
-    		$row[] = $company['name'];
-    		$row[] = $company['type'];
-    		$row[] = $company['face'];
-    		foreach($this->columns as $columnkey=>$column) {
-    			$marker = '';
-    			foreach($this->schedule as $id=>$shedule) {
-    				foreach ($shedule['periods'] as $period) {
-    					if ( ($period['columnValue'] == $columnkey) && ($period['rowValue'] == $compkey) ) {
-    						$marker = $shedule['time'];
-    					} 
-    				}
-    			}
-    			if ($marker == ''){
-	    			foreach($company['intersection'] as $id=>$m) {
-	    				if ( ($id == $columnkey) && (strlen(trim($m))>0) ) {
-	    					$marker = 'X';
-	    				}
-	    			}
-	    		}
-    			$row[] = $marker;
-    		}
-    		$data[] = $row;
-    	}
-    	return $data;
+     * @param void
+     * @return array
+     */
+    public function generateResultGrid()
+    {
+
+        $data = array();
+        $row = array('Company', 'Type', 'Priority' ,'User');
+        foreach ($this->columns as $c) {
+            $row[] = $c['name'];
+        }
+        $data[] = $row;
+        $row = array('persons:', '', '');
+        foreach ($this->columns as $c) {
+            $row[] = $c['persons'];
+        }
+        $data[] = $row;
+        foreach ($this->rows as $compkey => $company) {
+            $row = array();
+            $row[] = $company['name'];
+            $row[] = $company['type'];
+            $row[] = $company['face'];
+            foreach ($this->columns as $columnkey => $column) {
+                $marker = '';
+                foreach ($this->schedule as $id => $shedule) {
+                    foreach ($shedule['periods'] as $period) {
+                        if (($period['columnValue'] == $columnkey) && ($period['rowValue'] == $compkey)) {
+                            $marker = $shedule['time'];
+                        }
+                    }
+                }
+                if ($marker == '') {
+                    foreach ($company['intersection'] as $id => $m) {
+                        if (($id == $columnkey) && (strlen(trim($m)) > 0)) {
+                            $marker = 'X';
+                        }
+                    }
+                }
+                $row[] = $marker;
+            }
+            $data[] = $row;
+        }
+        return $data;
     }
 
 
